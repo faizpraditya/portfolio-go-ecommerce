63f8522c7bd6763a038acfb71659aa801ebf5929
pmf <faizpraditya7@gmail.com>
2024-02-09 06:56:30 +0700

    init routes

--- routes/routes.go
+++ routes/routes.go
@@ -1,1 +1,15 @@
 package routes
+
+import (
+	"github.com/faizpraditya/go-ecommerce/controllers"
+	"github.com/gin-gonic/gin"
+)
+
+func UserRoutes(incomingRoutes *gin.Engine) {
+	incomingRoutes.POST("/users/signup", controllers.SignUp())
+	incomingRoutes.POST("/users/login", controllers.Login())
+	incomingRoutes.POST("/admin/addproduct", controllers.ProductViewerAdmin())
+	incomingRoutes.GET("/users/productview", controllers.SearchProduct())
+	// just goin to search a keyword, no filtering etc in this project (at least initially)
+	incomingRoutes.GET("/users/search", controllers.SearchProductByQuery())
+}
