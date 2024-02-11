f366fd89261552ed1333f2234297c7ac02bf5204
pmf <faizpraditya7@gmail.com>
2024-02-12 01:56:56 +0700

    define func in controllers and database

--- controllers/address.go
+++ controllers/address.go
@@ -1,1 +1,19 @@
 package controllers
+
+import "github.com/gin-gonic/gin"
+
+func AddAddress() gin.HandlerFunc {
+	return nil
+}
+
+func EditHomeAddress() gin.HandlerFunc {
+	return nil
+}
+
+func EditWorkAddress() gin.HandlerFunc {
+	return nil
+}
+
+func DeleteAddrress() gin.HandlerFunc {
+	return nil
+}
--- controllers/controllers.go
+++ controllers/controllers.go
@@ -1,1 +1,31 @@
 package controllers
+
+import "github.com/gin-gonic/gin"
+
+func HashPassword(password string) string {
+	return ""
+}
+
+func VerifyPassword(userPassword string, givenPassword string) (bool, string) {
+	return true, ""
+}
+
+func SignUp() gin.HandlerFunc {
+	return nil
+}
+
+func Login() gin.HandlerFunc {
+	return nil
+}
+
+func ProductViewerAdmin() gin.HandlerFunc {
+	return nil
+}
+
+func SearchProduct() gin.HandlerFunc {
+	return nil
+}
+
+func SearchProductByQuery() gin.HandlerFunc {
+	return nil
+}
